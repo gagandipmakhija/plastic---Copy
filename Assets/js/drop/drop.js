82c0a84a542b3343a23253e64b53dd0b23344530
gagandipmakhija <gagandipmakhija@gmail.com>
2022-03-28 17:05:56 +0530

    commit

--- Assets/js/drop/drop.js
+++ Assets/js/drop/drop.js
@@ -18,10 +18,5 @@ const drop=()=>{
         $(".contact_details_services_toggle").css("display","none")
      
     })
-  
-
-  $(".patitent_cards_filter_outlay").on("mouseover",function(){
-      $("div.patitent_cards_filter_img::before").css("background","none")
-  })
 }
 export default drop;
