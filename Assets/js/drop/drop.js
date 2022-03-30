6bf45ba7cbcb57a6099f271d9f9ccc4519dff600
gagandipmakhija <gagandipmakhija@gmail.com>
2022-03-30 12:29:16 +0530

    commit

--- Assets/js/drop/drop.js
+++ Assets/js/drop/drop.js
@@ -18,5 +18,30 @@ const drop=()=>{
         $(".contact_details_services_toggle").css("display","none")
      
     })
+
+    document.querySelector(".header_nav_paras").addEventListener("mouseover",function(){
+        document.querySelector(".header_nav_paras_drop").style.display="block"
+        document.querySelector(".header_nav_paras_drop").addEventListener("mouseleave",function(){
+            document.querySelector(".header_nav_paras_drop").style.display="none"
+        })
+    })
+
+    
+    document.querySelector(".header_nav_paras1").addEventListener("mouseover",function(){
+        document.querySelector(".header_nav_paras1_drop1").style.display="block"
+        document.querySelector(".header_nav_paras1_drop1").addEventListener("mouseleave",function(){
+            document.querySelector(".header_nav_paras1_drop1").style.display="none"
+        })
+    })
+
+      
+    document.querySelector(".header_nav_paras2").addEventListener("mouseover",function(){
+        document.querySelector(".header_nav_paras2_drop2").style.display="block"
+        document.querySelector(".header_nav_paras2_drop2").addEventListener("mouseleave",function(){
+            document.querySelector(".header_nav_paras2_drop2").style.display="none"
+        })
+    })
+   
+   
 }
 export default drop;
